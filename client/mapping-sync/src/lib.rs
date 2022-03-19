f29b89fcb6d1ad231ac54796616ba15850539ab9
Alex Won <dnjscksdn98@gmail.com>
2022-03-19 19:57:10 +0900

    remove sync_from mapping-sync parameter

--- client/mapping-sync/src/lib.rs
+++ client/mapping-sync/src/lib.rs
@@ -98,7 +98,6 @@ pub fn sync_one_block<Block: BlockT, C, B>(
 	client: &C,
 	substrate_backend: &B,
 	frontier_backend: &fc_db::Backend<Block>,
-	sync_from: <Block::Header as HeaderT>::Number,
 	strategy: SyncStrategy,
 ) -> Result<bool, String>
 where
@@ -113,20 +112,25 @@ where
 		if leaves.is_empty() {
 			return Ok(false);
 		}
+
 		current_syncing_tips.append(&mut leaves);
 	}
 
-	let mut operating_header = None;
+	let mut operating_tip = None;
+
 	while let Some(checking_tip) = current_syncing_tips.pop() {
-		if let Some(checking_header) =
-			fetch_header(substrate_backend, frontier_backend, checking_tip, sync_from)?
+		if !frontier_backend
+			.mapping()
+			.is_synced(&checking_tip)
+			.map_err(|e| format!("{:?}", e))?
 		{
-			operating_header = Some(checking_header);
+			operating_tip = Some(checking_tip);
 			break;
 		}
 	}
-	let operating_header = match operating_header {
-		Some(operating_header) => operating_header,
+
+	let operating_tip = match operating_tip {
+		Some(operating_tip) => operating_tip,
 		None => {
 			frontier_backend
 				.meta()
@@ -134,6 +138,11 @@ where
 			return Ok(false);
 		}
 	};
+
+	let operating_header = substrate_backend
+		.header(BlockId::Hash(operating_tip))
+		.map_err(|e| format!("{:?}", e))?
+		.ok_or("Header not found".to_string())?;
 
 	if operating_header.number() == &Zero::zero() {
 		sync_genesis_block(client, frontier_backend, &operating_header)?;
@@ -163,7 +172,6 @@ pub fn sync_blocks<Block: BlockT, C, B>(
 	substrate_backend: &B,
 	frontier_backend: &fc_db::Backend<Block>,
 	limit: usize,
-	sync_from: <Block::Header as HeaderT>::Number,
 	strategy: SyncStrategy,
 ) -> Result<bool, String>
 where
@@ -174,37 +182,9 @@ where
 	let mut synced_any = false;
 
 	for _ in 0..limit {
-		synced_any = synced_any
-			|| sync_one_block(
-				client,
-				substrate_backend,
-				frontier_backend,
-				sync_from,
-				strategy,
-			)?;
+		synced_any =
+			synced_any || sync_one_block(client, substrate_backend, frontier_backend, strategy)?;
 	}
 
 	Ok(synced_any)
-}
-
-pub fn fetch_header<Block: BlockT, B>(
-	substrate_backend: &B,
-	frontier_backend: &fc_db::Backend<Block>,
-	checking_tip: Block::Hash,
-	sync_from: <Block::Header as HeaderT>::Number,
-) -> Result<Option<Block::Header>, String>
-where
-	B: sp_blockchain::HeaderBackend<Block> + sp_blockchain::Backend<Block>,
-{
-	if frontier_backend.mapping().is_synced(&checking_tip)? {
-		return Ok(None);
-	}
-
-	match substrate_backend.header(BlockId::Hash(checking_tip)) {
-		Ok(Some(checking_header)) if checking_header.number() >= &sync_from => {
-			Ok(Some(checking_header))
-		}
-		Ok(Some(_)) => Ok(None),
-		Ok(None) | Err(_) => Err("Header not found".to_string()),
-	}
 }
--- client/mapping-sync/src/worker.rs
+++ client/mapping-sync/src/worker.rs
@@ -26,8 +26,10 @@ use log::debug;
 use sc_client_api::{BlockOf, ImportNotifications};
 use sp_api::ProvideRuntimeApi;
 use sp_blockchain::HeaderBackend;
-use sp_runtime::traits::{Block as BlockT, Header as HeaderT};
+use sp_runtime::traits::Block as BlockT;
 use std::{pin::Pin, sync::Arc, time::Duration};
+
+const LIMIT: usize = 8;
 
 #[derive(PartialEq, Copy, Clone)]
 pub enum SyncStrategy {
@@ -45,12 +47,9 @@ pub struct MappingSyncWorker<Block: BlockT, C, B> {
 	frontier_backend: Arc<fc_db::Backend<Block>>,
 
 	have_next: bool,
-	retry_times: usize,
-	sync_from: <Block::Header as HeaderT>::Number,
+
 	strategy: SyncStrategy,
 }
-
-impl<Block: BlockT, C, B> Unpin for MappingSyncWorker<Block, C, B> {}
 
 impl<Block: BlockT, C, B> MappingSyncWorker<Block, C, B> {
 	pub fn new(
@@ -59,8 +58,6 @@ impl<Block: BlockT, C, B> MappingSyncWorker<Block, C, B> {
 		client: Arc<C>,
 		substrate_backend: Arc<B>,
 		frontier_backend: Arc<fc_db::Backend<Block>>,
-		retry_times: usize,
-		sync_from: <Block::Header as HeaderT>::Number,
 		strategy: SyncStrategy,
 	) -> Self {
 		Self {
@@ -73,8 +70,7 @@ impl<Block: BlockT, C, B> MappingSyncWorker<Block, C, B> {
 			frontier_backend,
 
 			have_next: true,
-			retry_times,
-			sync_from,
+
 			strategy,
 		}
 	}
@@ -122,8 +118,7 @@ where
 				self.client.as_ref(),
 				self.substrate_backend.blockchain(),
 				self.frontier_backend.as_ref(),
-				self.retry_times,
-				self.sync_from,
+				LIMIT,
 				self.strategy,
 			) {
 				Ok(have_next) => {
